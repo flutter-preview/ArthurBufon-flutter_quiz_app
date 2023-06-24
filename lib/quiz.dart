b60a775d7f2b5a02ffe6495d3165753f5a1c2aae
Arthur <arthurbufon2052@gmail.com>
2023-06-24 10:52:28 -0300

    renderizando conteudo condicionalmente

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,28 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app/questions_screen.dart';
 import 'package:flutter_quiz_app/start_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
 
   @override
-  State<Quiz> createState(){
+  State<Quiz> createState() {
     return _QuizState();
   }
 }
 
 class _QuizState extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
   @override
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 102, 0, 192),
-              Color.fromARGB(255, 115, 44, 237),
-            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 102, 0, 192),
+                Color.fromARGB(255, 115, 44, 237),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
-          child: const StartScreen(),
+          child: activeScreen,
         ),
       ),
     );
