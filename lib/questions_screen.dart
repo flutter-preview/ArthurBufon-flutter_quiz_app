0a624f01740db4edfa58a16f22f98b5e7eeea311
Arthur <arthurbufon2052@gmail.com>
2023-07-02 15:45:46 -0300

    passando dados entre widgets

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -4,7 +4,9 @@ import 'data/questions.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key});
+  const QuestionsScreen({super.key, required this.onSelectAnswer});
+
+  final void Function(String answer) onSelectAnswer;
 
   @override
   State<QuestionsScreen> createState() {
@@ -15,7 +17,8 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   var currentQuestionIndex = 0;
 
-  void answerQuestion() {
+  void answerQuestion(String selectedAnswer) {
+    widget.onSelectAnswer(selectedAnswer);
     setState(() {
       currentQuestionIndex++;
     });
@@ -36,7 +39,7 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             Text(
               currentQuestion.text,
               style: GoogleFonts.lato(
-                color: Color.fromARGB(255, 224, 186, 255),
+                color: const Color.fromARGB(255, 224, 186, 255),
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
               ),
@@ -46,7 +49,9 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             ...currentQuestion.getShuffledAnswers().map((answer) {
               return AnswerButton(
                 answerText: answer,
-                onTap: answerQuestion,
+                onTap: (){
+                  answerQuestion(answer);
+                },
               );
             }),
           ],
